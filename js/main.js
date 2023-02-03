3e5114a8990f48af8d54d151af986f8226980da7
IsaacFitzer <fitzerisaac@gmail.com>
2023-02-03 18:19:32 -0500

    game initialized, deck shuffled and rendered

--- js/main.js
+++ js/main.js
@@ -6,10 +6,40 @@
 
 
   /*----- cached elements  -----*/
-
+const newGameButton =  document.querySelector('button')
+const deckContainers = document.querySelectorAll('#deck > div')
+const completeContainers = document.querySelectorAll('#complete > div')
+const bottomRowContainers = document.querySelectorAll('#bottomRow > div')
 
   /*----- event listeners -----*/
-
+newGameButton.addEventListener('click', () => startNewGame())
 
   /*----- functions -----*/
+function startNewGame() {
+    newGameButton.style.visibility = 'hidden'
+    document.querySelectorAll('.cardContainer').forEach(container => container.innerHTML = '')
+    console.log(deckContainers[0])
+    getShuffledDeck().forEach(card => deckContainers[0].innerHTML += `<div class="card back ${card.face}"></div>`)
+    dealCards()
+}
 
+function dealCards() {
+    // write code here
+}
+
+function getShuffledDeck() {
+    const tempDeck = []
+    suits.forEach(suit => {
+        ranks.forEach(rank => {
+            tempDeck.push({
+                face: suit + rank,
+                value: Number(rank) || (rank === 'J' ? 11 : rank === 'Q' ? 12 : rank === 'K' ? 13 : 1)
+            })
+        })
+    })
+    const shuffledDeck = []
+    while (tempDeck.length > 0) {
+        shuffledDeck.push(tempDeck.splice(Math.floor(Math.random() * tempDeck.length), 1)[0])
+    }
+    return shuffledDeck
+}
